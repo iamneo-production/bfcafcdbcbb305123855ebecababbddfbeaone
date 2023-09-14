fa0f1bdca3b0fe81d62dd5f8a8298d25eedcaeb3
Iamneo <techadmin@iamneo.ai>
2023-09-14 08:09:12 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/EmployeeController.java
+++ springapp/src/main/java/com/examly/springapp/controller/EmployeeController.java
@@ -1,4 +1,14 @@
 @RestController
 public class EmployeeController {
     
+
+    @GetMapping("/api/salary/higher-than/{salary}")
+    public List<employee> getEmployeesWithSalaryHigherThan(@RequestParam("salary") float psalary)
+    {
+
+    }
+
+
+    
+
 }
