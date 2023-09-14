67bfcda176488cf625412463b13db258c58acea4
Iamneo <techadmin@iamneo.ai>
2023-09-14 08:03:22 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/repository/EmployeeRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/EmployeeRepository.java
@@ -1,5 +1,14 @@
 
 @Repository
 public class EmployeeRepository {
-    
+    @PersistenceContext
+	private EntityManager em;
+
+
+    public List<Employee> findEmpList() {    
+		TypedQuery<Employee> qry = em.createQuery("select e from Employee e where e.salary >?",Employee.class);
+     	List<Employee> empList = qry.setParameter(1,salary).getResultList();
+		return empList;
+	}
+
 }
